3179bcfa2be38f712544f085d49b868c7ab5020d
Lynneker Souza <lnksouzacm@gmail.com>
2021-09-01 22:46:51 -0300

    V1.0.2

---
+++ Array+Utils.swift
@@ -0,0 +1,14 @@
+//
+//  Array+Utils.swift
+//  PrivateUtils
+//
+//  Created by Lynneker Souza on 01/09/21.
+//
+
+import Foundation
+
+extension Array {
+    func get(_ at: Int) -> Bool {
+        return self.count < at
+    }
+}
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,5 +1,4 @@
 import XCTest
-import PrivateUtils
 
 class Tests: XCTestCase {
     
